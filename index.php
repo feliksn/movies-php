bce671e284b66cb16e09c0e4fd56a7625cb319f3
Feliks N <fnow01@gmail.com>
2024-03-13 17:21:38 +0100

    Add the DB connect function to index file

--- index.php
+++ index.php
@@ -6,21 +6,11 @@
     include "header.php";
     
 
-    // Проверяем подключение с нашой базой данный и получаем данные из базы.
-    // Это временный код. Он будет изменен. Пока что создаем быстрое подключение и проверяем работает ли база данных
-    // Все настройки подключения к базе данных находяться в файле function.php
-    $sql = "SELECT id, name, year FROM test";
-    
-    $result = $conn->query($sql);
+    // Получаем данные из базы данных по запросу sql
+    $data = getDBdata("SELECT * FROM test");
+    // Проверяем что записано в переменной $data
+    print("<pre>". print_r($data,true). "</pre>");   
 
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "Movie id: " . $row["id"]. "; Movie name: " . $row["name"]. "; Movie year: " . $row["year"]. "<br>";
-        }
-    } else {
-        echo "0 results";   
-    }
 
     // В главный файл включаем footer.php
     // В footer.php будет все то, что подключаеться после того как прорисовался сайт. js-скрипты, разные библиотеки js и прочее. То что должно быть в конце каждой страницы
