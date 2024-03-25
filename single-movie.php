e10ff7aced13ab916e514ce5b1935b9d58cd823b
Feliks N <fnow01@gmail.com>
2024-03-25 15:26:00 +0100

    Remove a loop for a single movie page

--- single-movie.php
+++ single-movie.php
@@ -1,33 +1,21 @@
 <?php
     include 'header.php';
-    // Получаем параметр id фильма из адреснной строки браузера "https://movies-phpmysql/single-movie.php?id=..."
-    $id = $_GET["id"];
-    
-    // Задача 3.3
-    // Вызвать функцию получения данных фильма по параметру $id и записать ее в переменную $movie
-    // $movie = фукцния_получения_данных_отдельного_фильма($id)
-    $movie = getMovieData($id);
+    $movie = getMovieData();
 ?>
 
-<!-- Задача 3.4
-Добавить данные фильма в разметку хтмл из переменной $movie -->
-<div class="container">
-<?php foreach($movie as $mov) { ?>
+<div class="container py-4">
     <div class="row">
-        <div class="col-4">
-            <img src="./content/movies-thumbnails/<?php echo $mov["thumbnail"]; ?>" alt="Movie thumbnail">
+        <div class="col-3 offset-1">
+            <img src="./content/movies-thumbnails/<?php echo $movie["thumbnail"]; ?>" alt="Movie thumbnail" class="d-block w-100">
         </div>
-        <div class="col-8">
-            <h5>
-               <?php echo $mov["title"]; ?><br>
-                <small>(<?php echo $mov["year"]; ?>)</small>
-            </h5>
-            <p><?php echo $mov["genres"]; ?></p>
-            <p><?php echo $mov["cast"]; ?></p>
-            <p><?php echo $mov["extract"]; ?></p>
+        <div class="col-7">
+            <h1><?php echo $movie["title"]; ?><br></h1>
+            <p class="fs-4 text-secondary">(<?php echo $movie["year"]; ?>)</p>
+            <p><?php echo $movie["genres"]; ?></p>
+            <p><?php echo $movie["cast"]; ?></p>
+            <p><?php echo $movie["extract"]; ?></p>
         </div>
     </div>
-    <?php } ?>
 </div>
 
 <?php
