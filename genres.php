acd4ecea5aed777673c6f6c5b10e765eda76f03f
vieri <egordohlyj4@gmail.com>
2024-03-25 21:45:40 +0300

    add list genres on page genres.php

--- genres.php
+++ genres.php
@@ -1,16 +1,19 @@
 <?php 
-    include "header.php"
+    include "header.php";
+    $genres = getGenresData();
     // Задача 4.1 - Создать функцию getGernesData() в fucntions.php которая получает название всех жанров
     // Задача 4.2 - Вызвать эту функцию и записать ее результат в переменную $genres в genres.php 
     // Задача 4.3 - В genres.php сделать список всех жанров, чтобы при открытии в браузере страницы genres.php, можно было видеть сслыки всех жарнов  
 ?>
 
 <div class="container py-3">
-    <div class="row">
-        <div class="col">
-            <a href="">A genres name</a>
+    <?php foreach($genres as $genre) { ?>
+        <div class="row">
+            <div class="col">
+                <a href=""><?php echo $genre['name']; ?></a>
+            </div>
         </div>
-    </div>
+    <?php } ?>
 </div>
 
 <?php
