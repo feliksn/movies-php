103faad5849499ba24712b22875914bef4dbffb4
Feliks N <fnow01@gmail.com>
2024-03-29 11:03:49 +0100

    Revert "alternativa html in genres.php"
    This reverts commit a1d4ecceaa52edffb5d131b00efd6f5cd4076d78.

--- genres.php
+++ genres.php
@@ -5,19 +5,60 @@
 ?>
 
 <div class="container py-3">
-  <div class="row">
-    <?php for($i = 0; $i<count($genresCols); $i++) { ?>
-      <div class='col'>
-        <?php for($j = 0; $j<count($genresCols[$i]); $j++) { ?>
-          <div class="row">
-            <a href="" class="fs-5 link-offset-1 link-underline link-underline-opacity-25">
-              <?php echo $genresCols[$i][$j]['name']; ?>
-            </a>
-          </div>
+    <!-- Для правильного отображения жанров, так как задуманно, используем схему сеток bootstrap с определенными классами -->
+    <!--     
+    .row
+    |-- >.col
+           |
+           >--.row
+                |
+                >--.col
+                    |
+                    -- Action
+                    -- Adventure
+                    -- Animated
+                    --...
+                >--.col
+                    |
+                    --Family
+                    --Fantasy
+                    --...
+    |--  >.col
+            |
+            >--.row
+                |
+                >--.col
+                    |
+                    --Noir
+                    --Perfomence
+                    --Political
+                    --...
+                >--.col
+                     |
+                     --Spy
+                     --Superhero
+                     --...
+    -->
+    <div class="row">
+        <!-- Запускаем цикл и проходимся по каждой колонке -->
+        <?php for($colIndex = 0; $colIndex < count($genresCols); $colIndex++) { ?>
+            <!-- Согласно схеме выше .col x 2 надо обернуь в .col>.row -->
+            <!-- Делаем условие при котором если индекс колонки в массиве четный (в нашем случае 0 или 2), Добавляем дополнительную хтмл разметку -->
+            <?php if($colIndex % 2 == 0) echo "<div class='col'><div class='row row-cols-1 row-cols-md-2'>"?>
+            <!-- Далее добавляем каждую колонку из массива -->
+            <div class="col">
+                <!-- Запускаем цикл и проходимся по каждому жанру в колонке -->
+                <?php for($genreIndexInCol = 0; $genreIndexInCol < count($genresCols[$colIndex]); $genreIndexInCol++) { ?>
+                    <!-- Отображаем имя жанра в сслыке с хтмл переносом <br>, т.к. ссылка не блочный элемент, а строчный -->
+                    <a href="" class="fs-5 link-offset-1 link-underline link-underline-opacity-25">
+                        <?php echo $genresCols[$colIndex][$genreIndexInCol]['name']; ?>
+                    </a><br>
+                <?php } ?>
+            </div>
+            <!-- Если индекс колонки нечетный закрываем хтмл элементы .col>.row -->
+            <?php if($colIndex % 2 !== 0) echo "</div></div>"?>
         <?php } ?>
-      </div>
-    <?php } ?>
-  </div>
+    </div>
 </div>
 
 <?php
