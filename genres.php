d1d00d34c41854639ada35a35aeb1d34b02a6112
Feliks N <fnow01@gmail.com>
2024-04-02 16:55:58 +0200

    Add cols order for genres grid

--- genres.php
+++ genres.php
@@ -1,62 +1,20 @@
 <?php 
     include "header.php";
     // Фукния возвращает массив из 4 колонок. Каждая колонка это массив из 11элементов (41/4 = 10.25 = сeil(10.25) = 11)
-    $genresCols = getGenresColsData();
+    $cols = getGenresColsData();
+    $colsOrderSM = [1,3,2,4];
 ?>
 
 <div class="container py-3">
-    <!-- Для правильного отображения жанров, так как задуманно, используем схему сеток bootstrap с определенными классами -->
-    <!--     
-    .row
-    |-- >.col
-           |
-           >--.row
-                |
-                >--.col
-                    |
-                    -- Action
-                    -- Adventure
-                    -- Animated
-                    --...
-                >--.col
-                    |
-                    --Family
-                    --Fantasy
-                    --...
-    |--  >.col
-            |
-            >--.row
-                |
-                >--.col
-                    |
-                    --Noir
-                    --Perfomence
-                    --Political
-                    --...
-                >--.col
-                     |
-                     --Spy
-                     --Superhero
-                     --...
-    -->
-    <div class="row">
-        <!-- Запускаем цикл и проходимся по каждой колонке -->
-        <?php for($colIndex = 0; $colIndex < count($genresCols); $colIndex++) { ?>
-            <!-- Согласно схеме выше .col x 2 надо обернуь в .col>.row -->
-            <!-- Делаем условие при котором если индекс колонки в массиве четный (в нашем случае 0 или 2), Добавляем дополнительную хтмл разметку -->
-            <?php if($colIndex % 2 == 0) echo "<div class='col'><div class='row row-cols-1 row-cols-md-2'>"?>
-            <!-- Далее добавляем каждую колонку из массива -->
-            <div class="col">
-                <!-- Запускаем цикл и проходимся по каждому жанру в колонке -->
-                <?php for($genreIndexInCol = 0; $genreIndexInCol < count($genresCols[$colIndex]); $genreIndexInCol++) { ?>
-                    <!-- Отображаем имя жанра в сслыке с хтмл переносом <br>, т.к. ссылка не блочный элемент, а строчный -->
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4">
+        <?php foreach($cols as $colIndex => $col) { ?>
+            <div class="col <?php echo "order-sm-$colsOrderSM[$colIndex] order-lg-$colIndex" ?>">
+                <?php foreach($col as $genreIndex => $genre) { ?>
                     <a href="" class="fs-5 link-offset-1 link-underline link-underline-opacity-25">
-                        <?php echo $genresCols[$colIndex][$genreIndexInCol]['name']; ?>
+                        <?php echo $genre['name']; ?>
                     </a><br>
                 <?php } ?>
             </div>
-            <!-- Если индекс колонки нечетный закрываем хтмл элементы .col>.row -->
-            <?php if($colIndex % 2 !== 0) echo "</div></div>"?>
         <?php } ?>
     </div>
 </div>
