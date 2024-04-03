44f4734fb1f768634f4a36d908ac4c7494f50041
vieri <egordohlyj4@gmail.com>
2024-04-03 22:54:08 +0300

    create link genres

--- single-movie.php
+++ single-movie.php
@@ -1,6 +1,8 @@
 <?php
     include 'header.php';
     $movie = getMovieData();
+    $genre = getGenreStrToMass();
+    $genre_link_id = getGenreLinkData();
 ?>
 
 <div class="container py-4">
@@ -12,7 +14,12 @@
             <h1><?php echo $movie["title"]; ?><br></h1>
             <p class="fs-4 text-secondary">(<?php echo $movie["year"]; ?>)</p>
             <!-- Задача 7 - Переделать текст жанров на ссылки, которые будут вести на отдельную страницу жанра -->
-            <p><?php echo $movie["genres"]; ?></p>
+            <p>
+                <?php for($i = 0; $i < count($genre); $i++) { ?>
+                    <a href="./single-genre.php?id=<?php echo $genre_link_id[$i] ?>"> <?php echo $genre[$i] ?> </a>
+                <?php } ?>
+            </p>
+
             <p><?php echo $movie["cast"]; ?></p>
             <p><?php echo $movie["extract"]; ?></p>
         </div>
