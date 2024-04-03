2eeee7ef920d4afb4dab344796f3ed618d6db41e
Feliks N <fnow01@gmail.com>
2024-04-03 11:02:25 +0200

    Change var name to more readible names

--- genres.php
+++ genres.php
@@ -1,17 +1,16 @@
 <?php 
     include "header.php";
-    // Фукния возвращает массив из 4 колонок. Каждая колонка это массив из 11элементов (41/4 = 10.25 = сeil(10.25) = 11)
     $cols = getGenresColsData();
     $colsOrderSM = [0,2,1,3];
 ?>
 
 <div class="container py-3">
     <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4">
-        <?php foreach($cols as $colIndex => $col) { ?>
+        <?php foreach($cols as $colIndex => $colGenres) { ?>
             <div class="col <?php echo "order-sm-$colsOrderSM[$colIndex] order-lg-$colIndex" ?>">
-                <?php foreach($col as $genre) { ?>
+                <?php foreach($colGenres as $genre) { ?>
                     <a href="" class="fs-5 link-offset-1 link-underline link-underline-opacity-25">
-                        <?php echo $genre['name']; ?>
+                        <?php echo $genre["name"]; ?>
                     </a><br>
                 <?php } ?>
             </div>
