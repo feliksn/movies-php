f4b0ddc082f4e9ed03538b36075c6bd6b22090ff
vieri <egordohlyj4@gmail.com>
2024-04-08 16:45:49 +0300

    replace comment

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ function getDBdata($sql)
     $conn = mysqli_connect($servername, $username, $password, $dbname);
 
     if (!$conn) {
-        die ("Не удалось подлючиться к базе данных: " . mysqli_connect_error());
+        die("Не удалось подлючиться к базе данных: " . mysqli_connect_error());
     }
 
     $data = $conn->query($sql);
@@ -32,16 +32,18 @@ function getDBdata($sql)
 // Функция показывает данные, записанные в переменной, которую можно передевать как параметр функции
 function showRawData($data)
 {
-    print ("<pre>" . print_r($data, true) . "</pre>");
+    print("<pre>" . print_r($data, true) . "</pre>");
 }
 
 // Функция возвращает обрезанную строку с "..." если ее длина превышает определенное кол-во символов 
-function getShortStr($str, $maxLen){
+function getShortStr($str, $maxLen)
+{
     return strlen($str) > $maxLen ? substr($str, 0, $maxLen) . "..." : $str;
 }
 
 // Функция возвращает данные всех фильмов для главной страницы
-function getMoviesData(){
+function getMoviesData()
+{
     $rows = getDBdata("SELECT * FROM data ORDER BY id LIMIT 8");
     $rowIndex = 0;
     $result = [];
@@ -61,7 +63,8 @@ function getMoviesData(){
 }
 
 // Функция возвращает данные фильма по id параметру
-function getMovieData(){
+function getMovieData()
+{
     $id = $_GET["id"];
     $movie = getDBdata("SELECT * FROM data WHERE id = '$id'")[0];
     $genresSqlStr = '"' . str_replace(',', '","', $movie["genres"]) . '"';
@@ -70,25 +73,28 @@ function getMovieData(){
     $castsArray = getDBdata("SELECT name, id FROM cast WHERE name IN ($castsSqlStr)");
     $movie["genres"] = $genresArray;
     $movie["cast"] = $castsArray;
-    return $movie; 
-} 
+    return $movie;
+}
 
 // Функция возвращает genre фильма по id параметру
-function getGenreData(){
+function getGenreData()
+{
     $id = $_GET["id"];
     $rows = getDBdata("SELECT * FROM genres WHERE id = '$id'");
     return $rows[0];
-} 
+}
 
 // Функция возвращает genre фильма из БД genres
-function getGenresData(){
+function getGenresData()
+{
     $rows = getDBdata("SELECT * FROM genres ORDER BY name ASC");
     return $rows;
 }
 
 // Делаем отдельную фукнцию для сетки жанров чтобы не запутаться в действии каждой функции
 // Фукния возвращает массив из 4 колонок. Каждая колонка это массив из 11 элементов (41/4 = 10.25 = сeil(10.25) = 11)
-function getGenresColsData(){
+function getGenresColsData()
+{
     $data = getGenresData();
     $colsLen = 4;
     $rowsLen = ceil(count($data) / $colsLen);
@@ -96,9 +102,10 @@ function getGenresColsData(){
     return $result;
 }
 
-// Функция возвращает genre фильма по id параметру
-function getCastData(){
+// Функция возвращает cast фильма по id параметру
+function getCastData()
+{
     $id = $_GET["id"];
     $rows = getDBdata("SELECT * FROM cast WHERE id = '$id'");
     return $rows[0];
-} 
+}
