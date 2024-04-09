3a9b3d340235b5e162d6bac2de3975c5d4a23bd4
vieri <egordohlyj4@gmail.com>
2024-04-09 16:07:57 +0300

    the margins have been changed

--- genres.php
+++ genres.php
@@ -1,14 +1,14 @@
-<?php 
-    include "header.php";
-    $cols = getGenresColsData();
-    $colsOrderSM = [0,2,1,3];
+<?php
+include "header.php";
+$cols = getGenresColsData();
+$colsOrderSM = [0, 2, 1, 3];
 ?>
 
 <div class="container py-3">
     <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4">
-        <?php foreach($cols as $colIndex => $colGenres) { ?>
+        <?php foreach ($cols as $colIndex => $colGenres) { ?>
             <div class="col <?php echo "order-sm-$colsOrderSM[$colIndex] order-lg-$colIndex" ?>">
-                <?php foreach($colGenres as $genre) { ?>
+                <?php foreach ($colGenres as $genre) { ?>
                     <a href="./single-genre.php?id=<?php echo $genre["id"] ?>" class="fs-5 link-offset-1 link-underline link-underline-opacity-25">
                         <?php echo $genre["name"]; ?>
                     </a><br>
@@ -19,5 +19,5 @@
 </div>
 
 <?php
-    include "footer.php"
+include "footer.php"
 ?>
