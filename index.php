8a76c8b5806d1b031bd1aa6cf188d9b317a59b75
vieri <egordohlyj4@gmail.com>
2024-04-09 16:04:07 +0300

    the margins have been changed

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php 
-    include "header.php";
-    $movies = getMoviesData();
+<?php
+include "header.php";
+$movies = getMoviesData();
 ?>
 
 <!-- главный контейнер -->
@@ -10,29 +10,29 @@
 
     <!-- контейнер для фильмов -->
     <div id="movies-container" class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 mb-3 g-3">
-    <?php foreach($movies as $movie) { ?>
-        <div class="col">
-            <div class="card border border-0 shadow-sm">
-                <img src="./content/movies-thumbnails/<?php echo $movie["thumbnail"]; ?>" class="card-img-top" alt="Movie thumbnail">
-                <div class="card-header border border-0"><?php echo $movie["genres"]; ?></div>
-                <div class="card-body">
-                    <h5 class="card-title mb-3">
-                        <span><?php echo $movie["title"] ?></span>
-                        <small class="text-body-tertiary">(<?php echo $movie["year"]; ?>)</small>
-                    </h5>
-                    <h6 class="card-text mb-3 text-secondary"><em><?php echo $movie["cast"]; ?></em></h6>
-                    <p class="card-text"><?php echo $movie["extract"]; ?></p>
-                    <a href="./single-movie.php?id=<?php echo $movie["id"] ?>" class="btn btn-primary">
-                        Read more...
-                    </a>
+        <?php foreach ($movies as $movie) { ?>
+            <div class="col">
+                <div class="card border border-0 shadow-sm">
+                    <img src="./content/movies-thumbnails/<?php echo $movie["thumbnail"]; ?>" class="card-img-top" alt="Movie thumbnail">
+                    <div class="card-header border border-0"><?php echo $movie["genres"]; ?></div>
+                    <div class="card-body">
+                        <h5 class="card-title mb-3">
+                            <span><?php echo $movie["title"] ?></span>
+                            <small class="text-body-tertiary">(<?php echo $movie["year"]; ?>)</small>
+                        </h5>
+                        <h6 class="card-text mb-3 text-secondary"><em><?php echo $movie["cast"]; ?></em></h6>
+                        <p class="card-text"><?php echo $movie["extract"]; ?></p>
+                        <a href="./single-movie.php?id=<?php echo $movie["id"] ?>" class="btn btn-primary">
+                            Read more...
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
         <?php } ?>
     </div>
 </div>
 
 
-<?php 
-    include "footer.php";
+<?php
+include "footer.php";
 ?>
