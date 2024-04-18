2682e9a0a60d6bc720f19b4351e5c550cbf8c4d0
Feliks N <fnow01@gmail.com>
2024-04-18 12:33:20 +0200

    Добавить действие перехода по номеру сраницы

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
             </a>
         </li>
         <!-- ссылка на первую страницу  -->
-        <li class="page-item"><a class="page-link" href="#1">1</a></li>
+        <li class="page-item"><a class="page-link" href="./?page=1">1</a></li>
         <!-- пустая ссылка с тремя точками -->
         <li class="page-item disabled"><span class="page-link">...</span></li>
         <!-- тут будем менять номера страницы через js -->
@@ -29,12 +29,12 @@
 </nav>
 
 <!-- Поиск страницы -->
-<form class="row justify-content-center g-1">
+<form action="/" method="GET" class="row justify-content-center g-1">
     <div class="col-auto">
-        <button type="button" class="btn btn-primary mb-3" id="btnGoToPage">Go to page</button>
+        <button type="submit" class="btn btn-primary mb-3" id="btnGoToPage">Go to page</button>
     </div>
     <div class="col-auto">
-        <input type="text" class="form-control input-go-to-page" id="inputGoToPage">
+        <input type="text" name="page" class="form-control input-go-to-page" id="inputGoToPage">
     </div>
 </form>
 
