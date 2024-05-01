23a3e0453adc84e9f3b5b57d1d6381cd288a53d3
Feliks N <fnow01@gmail.com>
2024-05-01 19:34:31 +0200

    Использовать html шаблон фильмов для главной стр.

--- index.php
+++ index.php
@@ -7,31 +7,9 @@ $movies = getMovies();
 <div class="container">
     <!-- Нзвание страницы -->
     <h1>Movies</h1>
-
-    <!-- контейнер для фильмов -->
-    <div id="movies-container" class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 mb-3 g-3">
-        <?php foreach ($movies["movies"] as $movie) { ?>
-            <div class="col">
-                <div class="card border border-0 shadow-sm">
-                    <img src="./content/movies-thumbnails/<?php echo $movie["thumbnail"]; ?>" onError="this.src='./images/movie-default.png'" class="card-img-top" alt="Movie thumbnail">
-                    <div class="card-header border border-0"><?php echo $movie["genres"]; ?></div>
-                    <div class="card-body">
-                        <h5 class="card-title mb-3">
-                            <span><?php echo $movie["title"] ?></span>
-                            <small class="text-body-tertiary">(<?php echo $movie["year"]; ?>)</small>
-                        </h5>
-                        <h6 class="card-text mb-3 text-secondary"><em><?php echo $movie["cast"]; ?></em></h6>
-                        <p class="card-text"><?php echo $movie["extract"] ?></p>
-                        <a href="./single-movie.php?id=<?php echo $movie["id"]; ?>" class="btn btn-primary">
-                            Read more...
-                        </a>
-                    </div>
-                </div>
-            </div>
-        <?php } ?>
-    </div>
+    <!-- Цикл фильмов -->
+    <?php include "movies-loop.php"; ?>
 </div>
-
 
 <?php
 include "footer.php";
