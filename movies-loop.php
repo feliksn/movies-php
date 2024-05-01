9b50f749b375676d2e31594e99d0c6ce76fd441c
Feliks N <fnow01@gmail.com>
2024-05-01 19:18:12 +0200

    Добавить html шаблон фильмов для исп. на разных страницах

---
+++ movies-loop.php
@@ -0,0 +1,25 @@
+<!-- контейнер для фильмов -->
+<div id="movies-container" class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 mb-3 g-3">
+    <?php foreach ($movies["list"] as $movie) { ?>
+        <div class="col">
+            <div class="card border border-0 shadow-sm">
+                <img src="./content/movies-thumbnails/<?php echo $movie["thumbnail"]; ?>" onError="this.src='./images/movie-default.png'" class="card-img-top" alt="Movie thumbnail">
+                <div class="card-header border border-0"><?php echo $movie["genres"]; ?></div>
+                <div class="card-body">
+                    <h5 class="card-title mb-3">
+                        <span><?php echo $movie["title"] ?></span>
+                        <small class="text-body-tertiary">(<?php echo $movie["year"]; ?>)</small>
+                    </h5>
+                    <h6 class="card-text mb-3 text-secondary"><em><?php echo $movie["cast"]; ?></em></h6>
+                    <p class="card-text"><?php echo $movie["extract"] ?></p>
+                    <a href="./single-movie.php?id=<?php echo $movie["id"]; ?>" class="btn btn-primary">
+                        Read more...
+                    </a>
+                </div>
+            </div>
+        </div>
+    <?php } ?>
+</div>
+
+<!-- Пагинация для фильмов -->
+<?php include "pagination.php"; ?>
--- single-genre.php
+++ single-genre.php
@@ -7,28 +7,7 @@ $genre_movies = getMoviesByIdList($genre['movies']);
 
 <div class="container">
     <h1><?php echo $genre["name"]; ?></h1>
-
-    <div id="genre-movies-container" class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 mb-3 g-3">
-        <?php foreach ($genre_movies as $genre_movie) { ?>
-            <div class="col">
-                <div class="card border border-0 shadow-sm">
-                    <img src="./content/movies-thumbnails/<?php echo $genre_movie["thumbnail"]; ?>" onError="this.src='./images/movie-default.png'" class="card-img-top" alt="Movie thumbnail">
-                    <div class="card-header border border-0"><?php echo $genre_movie["genres"]; ?></div>
-                    <div class="card-body">
-                        <h5 class="card-title mb-3">
-                            <span><?php echo $genre_movie["title"] ?></span>
-                            <small class="text-body-tertiary">(<?php echo $genre_movie["year"]; ?>)</small>
-                        </h5>
-                        <h6 class="card-text mb-3 text-secondary"><em><?php echo $genre_movie["cast"]; ?></em></h6>
-                        <p class="card-text"><?php echo $genre_movie["extract"]; ?></p>
-                        <a href="./single-movie.php?id=<?php echo $genre_movie["id"] ?>" class="btn btn-primary">
-                            Read more...
-                        </a>
-                    </div>
-                </div>
-            </div>
-        <?php } ?>
-    </div>
+    <?php include "movies-loop.php"; ?>
 </div>
        
 <?php
