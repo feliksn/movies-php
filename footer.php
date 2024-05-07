fc044ea80865697785ce7dc71906b471b4d85b2a
Feliks N <fnow01@gmail.com>
2024-05-07 09:51:03 +0200

    Создать глоб. переменную для работы с js скриптом в отдельном файле

--- footer.php
+++ footer.php
@@ -68,40 +68,11 @@ $pagination = getPagination($movies["page"], $movies["pages"]);
 </form>
 
 <script src="./lib/bootstrap/bootstrap.bundle.min.js"></script>
+<script>
+    // Перед скриптом script.js создаем переменную которая будет доступа в файле script.js
+    const MAX_PAGES = "<?php echo $movies["pages"]; ?>";
+</script>
 <script src="./script.js"></script>
 </body>
 
 </html>
-
-<!-- я не смог подключить его в отдельном файле, также не смог разобраться как сделать на  PHP / НА JS ты давал отличные уроки :) -->
-<script>
-    let max_pages = "<?php echo $movies["pages"]; ?>";
-    const btn = document.querySelector("#btnGoToPage");
-    const inp = document.querySelector("#inputGoToPage");
-    const form = document.querySelector("#formGoToPage");
-
-    const check = () => btn.disabled = inp.value <= 0  ||  inp.value > Number(max_pages) || inp.value == ""
-    inp.addEventListener('input', check);
-    check();
-
-    if (max_pages == 1){
-        form.innerHTML = ""
-    }
-        btn.addEventListener("click", function() {
-            const inputes = inp.value;
-            const search = location.search
-            const id_search = search.substring(0, search.indexOf("&")) ? search.substring(0, search.indexOf("&")) : search;
-            const index = "?page=" + inputes;
-            const single = "&page=" + inputes;
-            if (location.href.substring(0, 23) == "http://movies-phpmysql/") {
-                location.href = index;
-            }
-            if (location.href.substring(23, 39) == "single-genre.php") {
-                location.href = id_search + single
-            }
-            if (location.href.substring(23, 39) == "single-actor.php") {
-                location.href = id_search + single
-            }
-        })
- 
-</script>
